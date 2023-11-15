86273aa89ef32bef7c06c11709739e83f47f0771
maxcru-student <149310141+maxcru-student@users.noreply.github.com>
2023-11-15 14:19:35 +0100

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import requests
 
-def main():
+def get_joke():
     response = requests.get("https://api.punapi.com/random")
     data = response.json()
     joke = data['content']
-    print(f"Joke: {joke}")
+    return joke
+
+def main():
+    # Définissez le nombre de blagues à afficher
+    num_jokes = 5
+
+    # Boucle pour obtenir et afficher plusieurs blagues
+    for _ in range(num_jokes):
+        joke = get_joke()
+        print(f"Joke: {joke}")
+        print("-" * 30)  # Ligne de séparation entre les blagues
 
 if __name__ == "__main__":
     main()
